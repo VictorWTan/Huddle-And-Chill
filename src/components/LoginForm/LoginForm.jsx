aa241c798715e348b91cae33a35acf0182bd3674
victortan <vtan8090@gmail.com>
2022-08-01 23:40:18 -0400

    Added text-black

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -45,8 +45,8 @@ export default function LoginForm({ setUser }) {
         :
       <div className="mt-6 flex flex-col" onSubmit={handleSubmit}>
           <form className="flex flex-col" autoComplete="off" >
-            <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" placeholder="Email" type="text" name="email" value={credentials.email} onChange={handleChange} required />
-            <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" placeholder="Password" type="password" name="password" value={credentials.password} onChange={handleChange} required />
+            <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" placeholder="Email" type="text" name="email" value={credentials.email} onChange={handleChange} required />
+            <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" placeholder="Password" type="password" name="password" value={credentials.password} onChange={handleChange} required />
             <button className="my-5 px-5 py-2 border border-gray-700 rounded-3xl w-1/3 self-center hover:scale-105 duration-300 text-sky-600" type="submit">Sign In</button>
           </form>
       </div>
--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -37,10 +37,10 @@ export default class SignupForm extends Component {
           <div>
             <div className="px-10">
               <form className="flex flex-col" autoComplete="off" onSubmit={this.handleSubmit}>
-                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} required />
-                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
-                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
-                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center" type="password" name="confirm" placeholder="Confirm"value={this.state.confirm} onChange={this.handleChange} required />
+                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} required />
+                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+                <input className="m-2 px-5 py-2 border border-gray-600 rounded-lg w-1/3 self-center text-black" type="password" name="confirm" placeholder="Confirm"value={this.state.confirm} onChange={this.handleChange} required />
                 <button className="my-5 px-5 py-2 rounded-3xl w-1/3 self-center bg-sky-600 hover:scale-105 duration-300" type="submit" disabled={disable}>Create Account</button>
               </form>
             </div>
